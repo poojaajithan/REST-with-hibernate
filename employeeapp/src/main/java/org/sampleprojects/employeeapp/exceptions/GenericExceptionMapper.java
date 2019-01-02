dff8ff69937d89ca0bafa423c8a501b7af6177b8
Pooja Ajithan <pajithan@oradev.oraclecorp.com>
2019-01-02 22:40:21 +0530

    Added Exception Handling

---
+++ employeeapp/src/main/java/org/sampleprojects/employeeapp/exceptions/GenericExceptionMapper.java
@@ -0,0 +1,20 @@
+package org.sampleprojects.employeeapp.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.sampleprojects.employeeapp.model.ErrorMessage;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable>{
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity(new ErrorMessage(exception.getMessage(),500))
+				.build();
+	}
+
+}
